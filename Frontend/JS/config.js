4a1c71b5bae91b312806350e2842b079b392f026
KernelPhilosopher <kernelphilosopher@gmail.com>
2025-09-25 21:07:46 -0500

    New Redirects

--- Frontend/JS/config.js
+++ Frontend/JS/config.js
@@ -6,12 +6,12 @@ const CONFIG = {
   API_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000/api"
-      : "https://tu-backend-app.onrender.com/api", // Reemplaza con tu URL de Render
+      : "https://post-place-backend-ayf9.onrender.com", // Reemplaza con tu URL de Render
 
   SOCKET_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000"
-      : "https://tu-backend-app.onrender.com", // Reemplaza con tu URL de Render
+      : "https://post-place-backend-ayf9.onrender.com", // Reemplaza con tu URL de Render
 };
 
 // Hacer CONFIG global
