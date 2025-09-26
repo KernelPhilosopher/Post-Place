311eec0ae8ca4a677202ac2f5b4a90434a31e365
KernelPhilosopher <kernelphilosopher@gmail.com>
2025-09-25 22:26:40 -0500

    Updated Config.js

--- Frontend/JS/config.js
+++ Frontend/JS/config.js
@@ -5,13 +5,13 @@
 const CONFIG = {
   API_URL:
     window.location.hostname === "localhost"
-      ? "http://localhost:3000/api"
-      : "https://post-place-backend-ayf9.onrender.com", // Reemplaza con tu URL de Render
+      ? "http://localhost:3000/api" // Nota el /api al final
+      : "https://post-place-backend-ayf9.onrender.com/api", // Y aquí también
 
   SOCKET_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000"
-      : "https://post-place-backend-ayf9.onrender.com", // Reemplaza con tu URL de Render
+      : "https://post-place-backend-ayf9.onrender.com",
 };
 
 // Hacer CONFIG global
